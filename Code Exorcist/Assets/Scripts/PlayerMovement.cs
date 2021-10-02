d8520c004dec7cd74cdd77f04b715c051996a3a6
Tom-Roman <tommy101800@gmail.com>
2021-10-02 16:59:26 -0400

    sus demo

--- Code Exorcist/Assets/Scripts/PlayerMovement.cs
+++ Code Exorcist/Assets/Scripts/PlayerMovement.cs
@@ -6,18 +6,24 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cam;
-    public Rigidbody rb;
 
-    public float speed = 6f;
+    public float speed;
+    public float jumpForce;
+    bool isGrounded = false ;
+    public float gravityScale = 1;
+
+    private Vector3 moveDirection;
 
     private void Start()
     {
-        rb = GetComponentInChildren<Rigidbody>();
+        moveDirection = new Vector3(0f, 0f, 0f);
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
+        GroundCheck();
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -27,11 +33,42 @@ public class PlayerMovement : MonoBehaviour
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            transform.Translate(moveDir.x * speed * Time.deltaTime, 0, moveDir.z * speed * Time.deltaTime);
-        }
-        if (Input.GetButtonDown("Jump"))
+            moveDirection.x = moveDir.x * speed;
+            moveDirection.z = moveDir.z * speed;
+        } else
         {
-            rb.AddForce(new Vector3(0, 7, 0), ForceMode.Impulse);
+            moveDirection.x = 0f;
+            moveDirection.z = 0f;
+        }
+
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            moveDirection.y = jumpForce;
+        }
+
+        if (!isGrounded)
+        {
+            moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
+        }
+
+        controller.Move(moveDirection * Time.deltaTime);
+
+
+    }
+    void GroundCheck()
+    {
+        RaycastHit hit;
+        float distance = 2f;
+        Vector3 dir = new Vector3(0, -1);
+
+        if (Physics.Raycast(transform.position, dir, out hit, distance))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
         }
     }
 }
